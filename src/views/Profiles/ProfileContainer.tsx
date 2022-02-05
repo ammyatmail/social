cf85e710851c367fb038d4a1996d0e189ca2c4d5
amit kumar <amit@preeostudios.com>
2022-02-05 18:20:40 +0100

    dd

--- src/views/Profiles/ProfileContainer.tsx
+++ src/views/Profiles/ProfileContainer.tsx
@@ -1,22 +1,45 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  Toolbar,
+  AppBar,
+  Button,
+  Badge,
+  IconButton,
+  Collapse,
+  InputBase,
+  Switch,
+  FormControlLabel,
+} from "@material-ui/core";
 import { DelayedLinearProgress, SnackbarContext } from "components";
 import { useApiClient } from "hooks";
 import * as React from "react";
 import { MasterProfileList } from "models/MasterProfileList";
 import { Pagination } from "@material-ui/lab";
 import { ProfileCard } from "./ProfileCard";
+import { Close, FilterList, Search } from "@material-ui/icons";
+import { useStyles } from "./styles";
 
-const useStyles = makeStyles((theme) => ({
-  pagination: {
-    display: "flex",
-    justifyContent: "center",
-    paddingTop: theme.spacing(2),
-  },
-}));
+enum AgeFilter {
+  ALL = "All",
+  Age_20_30 = "20 - 30 Years",
+  Age_30_40 = "31 - 40 Years",
+  Age_41 = "41 and above Years",
+}
+interface Filter {
+  single: boolean;
+  smoke: boolean;
+  online: boolean;
+}
 export const ProfileContainer: React.FC = () => {
   const classes = useStyles();
   const perPageDataLength = 20;
   const [pageNumber, setPage] = React.useState<number>(1);
+  const [filter, setFilter] = React.useState<Filter>({
+    smoke: false,
+    single: false,
+    online: false,
+  });
+
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
@@ -45,10 +68,108 @@ export const ProfileContainer: React.FC = () => {
       snackbar.error(errorProfile);
     }
   }, [errorProfile, snackbar]);
+  const [searchText, setSearchText] = React.useState<string>("");
+  const [filtersOpen, setFiltersOpen] = React.useState(false);
 
   return (
     <React.Fragment>
       <DelayedLinearProgress loading={profileLoading} />
+      <AppBar position="static" color="transparent" elevation={0}>
+        <Toolbar className={classes.applicationsToolbar}>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <IconButton
+                className={classes.searchBtn}
+                onClick={() => {
+                  if (searchText && searchText.length !== 0) {
+                    setSearchText("");
+                  }
+                }}
+              >
+                {searchText && searchText.length !== 0 ? <Close /> : <Search />}
+              </IconButton>
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              fullWidth
+              value={searchText}
+              autoFocus
+              onChange={(e) => {
+                setSearchText((e.target.value as string).toLowerCase());
+              }}
+            />
+          </div>
+          <div className={classes.badge}>
+            <Badge badgeContent={getFilterCount()} color="primary">
+              <Button
+                className={classes.filterBtn}
+                startIcon={<FilterList />}
+                onClick={() => {
+                  setFiltersOpen(!filtersOpen);
+                }}
+              >
+                Filter
+              </Button>
+            </Badge>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Collapse in={filtersOpen}>
+        <Grid container className={classes.gridContainer}>
+          <Grid item xs={12} sm={3} md={2} lg={2}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={filter?.online}
+                  onChange={(e) => {
+                    setFilter((curr) => ({
+                      ...curr,
+                      online: e.target.checked,
+                    }));
+                  }}
+                />
+              }
+              label="Online status"
+            />
+          </Grid>
+          <Grid item xs={12} sm={3} md={2} lg={2}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={filter?.single}
+                  onChange={(e) => {
+                    setFilter((curr) => ({
+                      ...curr,
+                      single: e.target.checked,
+                    }));
+                  }}
+                />
+              }
+              label="Single"
+            />
+          </Grid>
+          <Grid item xs={12} sm={3} md={2} lg={2}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={filter?.smoke}
+                  onChange={(e) => {
+                    setFilter((curr) => ({
+                      ...curr,
+                      smoke: e.target.checked,
+                    }));
+                  }}
+                />
+              }
+              label="Smoker"
+            />
+          </Grid>
+        </Grid>
+      </Collapse>
       <Grid container>
         {getPaginationData().map((item, index) => (
           <ProfileCard profileItem={item} key={index.toString()} />
@@ -65,4 +186,13 @@ export const ProfileContainer: React.FC = () => {
       </div>
     </React.Fragment>
   );
+  function getFilterCount() {
+    return Object.values(filter).filter((k) => {
+      if (!k) {
+        return false;
+      } else {
+        return true;
+      }
+    }).length;
+  }
 };
---
+++ src/views/Profiles/styles.ts
@@ -0,0 +1,70 @@
+import { alpha, makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  pagination: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: theme.spacing(2),
+  },
+  toolbar: {
+    justifyContent: "flex-end",
+    minHeight: 0,
+    padding: 0,
+    marginTop: theme.spacing(1),
+  },
+  removeIcon: {
+    color: theme.palette.danger.main,
+  },
+  applicationsToolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    padding: 0,
+  },
+  search: {
+    position: "relative",
+    color: "#fff",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+  },
+  checkbox: { paddingTop: theme.spacing(0) },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1000,
+  },
+  searchBtn: {
+    marginRight: 100,
+    pointerEvents: "auto",
+  },
+  badge: {
+    padding: theme.spacing(2.5),
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(5)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+  },
+  filterBtn: {
+    textTransform: "none",
+    color: theme.palette.common.white,
+  },
+  gridContainer: {
+    margin: theme.spacing(0, 0, 2, 0),
+    background: theme.palette.common.white,
+  },
+}));
